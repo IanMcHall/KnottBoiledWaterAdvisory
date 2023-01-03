494093c345537db661a9df0e6cac45e14036481c
Ian <ianmchall@yahoo.com>
2023-01-03 14:35:28 -0500

    second commit

--- Boiled Water Alert App/Program.cs
+++ Boiled Water Alert App/Program.cs
@@ -1,28 +1,123 @@
-﻿bool z41701 = false;
-bool z41640 = false;
-bool z41822 = false;
-bool z41844 = false;
-bool z41773 = false;
-bool z41817 = false;
-bool z41722 = false;
-bool z41630 = false;
-bool z41862 = false;
-bool z41740 = false;
-bool z41828 = false;
-bool z41725 = false;
-bool z41843 = false;
-bool z41847 = false;
-bool z41836 = false;
-bool z41831 = false;
-bool z41834 = false;
-bool z41839 = false;
-bool z41772 = false;
-bool z41859 = false;
-bool z41743 = false;
-bool z41759 = false;
-bool z41861 = false;
-bool z41643 = false;
+﻿using Boiled_Water_Alert_App;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
 
+Zip z41701 = new Zip();
+z41701.zipCode = "41701";
+z41701.status = false;
+
+Zip z41640 = new Zip();
+z41640.zipCode = "41640";
+z41640.status = false;
+
+Zip z41822 = new Zip();
+z41822.zipCode = "41822";
+z41822.status = false;
+
+Zip z41844 = new Zip();
+z41844.zipCode = "41844";
+z41844.status = false;
+
+Zip z41773 = new Zip();
+z41773.zipCode = "41773";
+z41773.status = false;
+
+Zip z41817 = new Zip();
+z41817.zipCode = "41817";
+z41817.status = false;
+
+Zip z41722 = new Zip();
+z41722.zipCode = "41722";
+z41722.status = false;
+
+Zip z41630 = new Zip();
+z41630.zipCode = "41630";
+z41630.status = false;
+
+Zip z41862 = new Zip();
+z41862.zipCode = "41862";
+z41862.status = false;
+
+Zip z41740 = new Zip();
+z41740.zipCode = "41740";
+z41740.status = false;
+
+Zip z41828 = new Zip();
+z41828.zipCode = "41828";
+z41828.status = false;
+
+Zip z41725 = new Zip();
+z41725.zipCode = "41725";
+z41725.status = false;
+
+Zip z41843 = new Zip();
+z41843.zipCode = "41843";
+z41843.status = false;
+
+Zip z41847 = new Zip();
+z41847.zipCode = "41847";
+z41847.status = false;
+
+Zip z41836 = new Zip();
+z41836.zipCode = "41836";
+z41836.status = false;
+
+Zip z41831 = new Zip();
+z41831.zipCode = "41831";
+z41831.status = false;
+
+Zip z41834 = new Zip();
+z41834.zipCode = "41834";
+z41834.status = false;
+
+Zip z41839 = new Zip();
+z41839.zipCode = "41839";
+z41839.status = false;
+
+Zip z41772 = new Zip();
+z41772.zipCode = "41772";
+z41772.status = false;
+
+Zip z41859 = new Zip();
+z41859.zipCode = "41859";
+z41859.status = false;
+
+Zip z41743 = new Zip();
+z41743.zipCode = "41743";
+z41743.status = false;
+
+Zip z41759 = new Zip();
+z41759.zipCode = "41759";
+z41759.status = false;
+
+Zip z41861 = new Zip();
+z41861.zipCode = "41861";
+z41861.status = false;
+
+Zip z41643 = new Zip();
+z41643.zipCode = "41643";
+z41643.status = false;
+
+List<Zip> zipCodes = new List<Zip>();
+    void WriteToCsv(List<Zip> zipCodes)
+    {
+    using (var writer = new StreamWriter("CurrentData.csv"))
+    using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+    {
+        csv.WriteRecords(zipCodes);
+    }
+    }
+ List<Zip> ReadFromCsv()
+{
+    var config = new CsvConfiguration(CultureInfo.InvariantCulture);
+    using (var reader = new StreamReader("CurrentData.csv"))
+    using (var csv = new CsvReader(reader, config))
+    {
+        var characters = csv.GetRecords<Zip>().ToList();
+        return zipCodes;
+    }
+}
 
 Console.WriteLine("Knott County Boiled Water Advisory Automated System");
 Console.WriteLine("Please select an option:");
@@ -62,76 +157,76 @@ void UpdateBoiledWaterAdvisoryAlerts()
     switch(userResponse)
     {
         case "41701":
-            z41701 = true;
+            z41701.status = true;
             break;
         case "41640":
-            z41640 = true;
+            z41640.status = true;
             break;
         case "41822":
-            z41822 = true;
+            z41822.status = true;
             break;
         case "41844":
-            z41844 = true;
+            z41844.status = true;
             break;
         case "41773":
-            z41773 = true;
+            z41773.status = true;
             break;
         case "41817":
-            z41817 = true;
+            z41817.status = true;
             break;
         case "41722":
-            z41722 = true;
+            z41722.status = true;
             break;
         case "41630":
-            z41630 = true;
+            z41630.status = true;
             break;
         case "41862":
-            z41862 = true;
+            z41862.status = true;
             break;
         case "41740":
-            z41740 = true;
+            z41740.status = true;
             break;
         case "41828":
-            z41828 = true;
+            z41828.status = true;
             break;
         case "41725":
-            z41725 = true;
+            z41725.status = true;
             break;
         case "41843":
-            z41843 = true;
+            z41843.status = true;
             break;
         case "41847":
-            z41847 = true;
+            z41847.status = true;
             break;
         case "41836":
-            z41836 = true;
+            z41836.status = true;
             break;
         case "41831":
-            z41831 = true;
+            z41831.status = true;
             break;
         case "41834":
-            z41834 = true;
+            z41834.status = true;
             break;
         case "41839":
-            z41839 = true;
+            z41839.status = true;
             break;
         case "41772":
-            z41772 = true;
+            z41772.status = true;
             break;
         case "41859":
-            z41859 = true;
+            z41859.status = true;
             break;
         case "41743":
-            z41743 = true;
+            z41743.status = true;
             break;
         case "41759":
-            z41759 = true;
+            z41759.status = true;
             break;
         case "41861":
-            z41861 = true;
+            z41861.status = true;
             break;
         case "41643":
-            z41643 = true;
+            z41643.status = true;
             break;
             default:
             Console.WriteLine("Invalid Area Code. Please try again.");
@@ -141,28 +236,28 @@ void UpdateBoiledWaterAdvisoryAlerts()
 
 void CheckCurrentStatus()
 {
-    Console.WriteLine("41701 = " + (z41701));
-    Console.WriteLine("41640 = " + (z41640));
-    Console.WriteLine("41822 = " + (z41822));
-    Console.WriteLine("41844 = " + (z41844));
-    Console.WriteLine("41773 = " + (z41773));
-    Console.WriteLine("41817 = " + (z41817));
-    Console.WriteLine("41722 = " + (z41722));
-    Console.WriteLine("41630 = " + (z41630));
-    Console.WriteLine("41862 = " + (z41862));
-    Console.WriteLine("41740 = " + (z41740));
-    Console.WriteLine("41828 = " + (z41828));
-    Console.WriteLine("41725 = " + (z41725));
-    Console.WriteLine("41843 = " + (z41843));
-    Console.WriteLine("41847 = " + (z41847));
-    Console.WriteLine("41836 = " + (z41836));
-    Console.WriteLine("41831 = " + (z41831));
-    Console.WriteLine("41834 = " + (z41834));
-    Console.WriteLine("41839 = " + (z41839));
-    Console.WriteLine("41772 = " + (z41772));
-    Console.WriteLine("41859 = " + (z41859));
-    Console.WriteLine("41743 = " + (z41743));
-    Console.WriteLine("41759 = " + (z41759));
-    Console.WriteLine("41861 = " + (z41861));
-    Console.WriteLine("41643 = " + (z41643));
+    Console.WriteLine("41701 = " + (z41701.status));
+    Console.WriteLine("41640 = " + (z41640.status));
+    Console.WriteLine("41822 = " + (z41822.status));
+    Console.WriteLine("41844 = " + (z41844.status));
+    Console.WriteLine("41773 = " + (z41773.status));
+    Console.WriteLine("41817 = " + (z41817.status));
+    Console.WriteLine("41722 = " + (z41722.status));
+    Console.WriteLine("41630 = " + (z41630.status));
+    Console.WriteLine("41862 = " + (z41862.status));
+    Console.WriteLine("41740 = " + (z41740.status));
+    Console.WriteLine("41828 = " + (z41828.status));
+    Console.WriteLine("41725 = " + (z41725.status));
+    Console.WriteLine("41843 = " + (z41843.status));
+    Console.WriteLine("41847 = " + (z41847.status));
+    Console.WriteLine("41836 = " + (z41836.status));
+    Console.WriteLine("41831 = " + (z41831.status));
+    Console.WriteLine("41834 = " + (z41834.status));
+    Console.WriteLine("41839 = " + (z41839.status));
+    Console.WriteLine("41772 = " + (z41772.status));
+    Console.WriteLine("41859 = " + (z41859.status));
+    Console.WriteLine("41743 = " + (z41743.status));
+    Console.WriteLine("41759 = " + (z41759.status));
+    Console.WriteLine("41861 = " + (z41861.status));
+    Console.WriteLine("41643 = " + (z41643.status));
 }
---
+++ Boiled Water Alert App/Zip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boiled_Water_Alert_App
+{
+
+    internal class Zip
+    {
+        public string zipCode;
+        public bool status;
+    }
+}
+
+
+
